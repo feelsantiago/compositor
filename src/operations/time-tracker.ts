a4f328aaa42b76a9abbb9f3d5793242248d72add
Filipe Santiago <porto.santiago@hotmail.com>
2023-03-20 14:51:52 -0300

    improve format

--- src/operations/time-tracker.ts
+++ src/operations/time-tracker.ts
@@ -2,7 +2,11 @@ import { OutputTimeStream, Result } from "../types";
 import { Runable } from "./operations";
 
 export class TimeTracker<T> implements Runable<T> {
-    constructor(private readonly key: string, private readonly delegate: Runable<T>, private output: OutputTimeStream = { time: console.time, timeEnd: console.timeEnd }) { }
+    constructor(
+        private readonly key: string,
+        private readonly delegate: Runable<T>,
+        private output: OutputTimeStream = { time: console.time, timeEnd: console.timeEnd }
+    ) { }
 
     public run(): Result<T> {
         this.output.time(this.key);
