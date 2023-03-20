b3c266abc330d8471bf58fd60c49d17f8fd26eab
Filipe Santiago <porto.santiago@hotmail.com>
2023-03-20 13:20:28 -0300

    add retryable

--- src/compositor.ts
+++ src/compositor.ts
@@ -13,7 +13,11 @@ export interface TimeTrackeable<T> {
     time(key: string): Matchable<T>;
 }
 
-export interface Composible<T> extends Runable<T>, Matchable<T>, TimeTrackeable<T> {
+export interface Retriable<T> {
+    retry(times: number): Matchable<T>;
+}
+
+export interface Composible<T> extends Runable<T>, Matchable<T>, TimeTrackeable<T>, Retriable<T> {
 }
 
 export class WrappedFunction<T> implements Runable<T> {
@@ -36,6 +40,28 @@ export class TimeTracker<T> implements Runable<T> {
     }
 }
 
+export class Retry<T> implements Runable<T> {
+    constructor(private readonly times: number, private readonly delegate: Runable<T>) { }
+
+    public run(): Result<T> {
+        let retry = this.times;
+        let fail: Result<T> = { ok: false, error: new Error('Retry fail') };
+
+        while (retry > 0) {
+            const result = this.delegate.run();
+
+            if (result.ok) {
+                return result;
+            }
+
+            fail = result;
+            retry -= 1;
+        }
+
+        return fail;
+    }
+}
+
 export class Compositor<T> implements Composible<T> {
     constructor(private readonly delegate: Runable<T>) { }
 
@@ -49,6 +75,10 @@ export class Compositor<T> implements Composible<T> {
 
     public time(key: string): Composible<T> {
         return new Compositor(new TimeTracker(key, this.delegate));
+    }
+
+    public retry(times: number): Composible<T> {
+        return new Compositor(new Retry(times, this.delegate));
     }
 
     public match<R>(matchers: Matchers<T, Error, R>): R {
--- tests/compositor.test.ts
+++ tests/compositor.test.ts
@@ -1,4 +1,4 @@
-import { Compositor, TimeTracker, WrappedFunction } from '../src/compositor';
+import { Compositor, Retry, TimeTracker, WrappedFunction } from '../src/compositor';
 import { OutputTimeStream } from '../src/types';
 
 describe('Compositor', () => {
@@ -38,4 +38,29 @@ describe('TimeTracker', () => {
         expect(mockTime.time).toHaveBeenCalledWith('key');
         expect(mockTime.timeEnd).toHaveBeenCalledWith('key');
     });
+});
+
+describe('Retry', () => {
+    it('Should retry if operation fail', () => {
+        const operation = jest.fn().mockImplementation(() => {
+            throw new Error('Mock Error');
+        });
+
+        const retry = new Retry(3, new WrappedFunction(operation));
+        const result = retry.run();
+
+        expect(operation).toHaveBeenCalledTimes(3);
+        expect(result.ok).toBeFalsy();
+        expect((result as any).error.toString()).toEqual(new Error('Mock Error').toString());
+    });
+
+    it('Should stop retry when success', () => {
+        const operation = jest.fn();
+
+        const retry = new Retry(3, new WrappedFunction(operation));
+        const result = retry.run();
+
+        expect(operation).toHaveBeenCalledTimes(1);
+        expect(result.ok).toBeTruthy();
+    })
 });
